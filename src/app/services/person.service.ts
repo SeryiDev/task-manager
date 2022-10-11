884ae5e342e13342471624393b2cd82ea1345c86
Sergio García Gómez <sergicodcraft@gmail.com>
2022-10-11 18:49:21 +0200

    Images and Nicks added

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -11,52 +11,62 @@ export class PersonService {
       id: 1, 
       name: 'Sergio', 
       surname: 'García', 
-      nickname:'SeryiGG',
+      nickname:'SeryiDev',
+      image: 'https://imgs.search.brave.com/Slwld_uZmt1XZjWrwlZtdlgPZJola-dViGxrLYb1KHA/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvcGVvcGxlLWF2/YXRhci1mbGF0LTEv/NjQvZ2xhc3Nlc19i/dXNpbmVzc21hbl9w/ZW9wbGVfbWFsZV9t/YW5fYXZhdGFyX2Js/b25kZS01MTIucG5n'
     },
     {
       id: 2, 
       name: 'Hanane', 
-      surname: 'Arrazouki'
+      surname: 'Arrazouki',
+      nickname:'No nickname'
     },
     {
       id: 3, 
-      name: 'Verónica', 
-      surname: 'González'
+      name: 'Juan Antonio', 
+      surname: 'García',
+      nickname:'Arrow'
     },
     {
       id: 4, 
-      name: 'Juan Antonio', 
-      surname: 'García'
+      name: 'Antonio', 
+      surname: 'Suárez',
+      nickname:'No nickname'
     },
     {
       id: 5, 
       name: 'Gonzalo José', 
-      surname: 'Salmerón'
+      surname: 'Salmerón',
+      nickname:'No nickname'
     },
     {
       id: 6, 
       name: 'Raúl', 
-      surname: 'Vargas'
+      surname: 'Vargas',
+      nickname:'El Mirlo'
     },
     {
       id: 7, 
       name: 'Adrián', 
-      surname: 'Dueñas'
+      surname: 'Dueñas',
+      nickname:'No nickname'
     },
     {
       id: 8, 
       name: 'Rubén', 
-      surname: 'Díaz'
+      surname: 'Díaz',
+      nickname:'No nickname'
     },
     {
       id: 9, 
       name: 'Natalia', 
-      surname: 'Contreras'
+      surname: 'Contreras',
+      nickname:'No nickname'
     },
     {
       id: 10, 
       name: 'Nesrin', 
-      surname: 'Edwan'
+      surname: 'Edwan',
+      nickname:'No nickname'
     }
   ]
 

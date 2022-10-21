901be8da73b52789f7aa45fdd64909a93a678293
Sergio García Gómez <sergicodcraft@gmail.com>
2022-10-21 02:27:36 +0200

    Assignment component updated

--- src/app/pages/assignment-management/assignment-management.page.ts
+++ src/app/pages/assignment-management/assignment-management.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Assignment } from 'src/app/core/models/assignment';
+import { AssignmentService } from 'src/app/core/services/assignment.service';
 
 @Component({
   selector: 'app-assignment-management',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentManagementPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private assignmentService: AssignmentService,
+    private alertController: AlertController
+    ) { }
 
   ngOnInit() {
   }
 
+  getAssignmentList() {
+    return this.assignmentService.getAssignmentsList();
+  }
+
+  deleteAssignmentByID(id: number) {
+    this.assignmentService.deleteAssignmentByID(id);
+  }
+
+  onNewAssignment() {
+
+  }
+
+  onEditAssignment(assignment: Assignment) {
+
+  }
+
+  onDeleteAssignment(assignment: Assignment) {
+    this.onDeleteAlert(assignment);
+  }
+
+  async onDeleteAlert(assignment: Assignment) {
+    const alert = await this.alertController.create({
+      mode: 'ios',
+      header: '¿Are you sure you want to delete this assignment?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+          },
+        },
+        {
+          text: 'Yes',
+          role: 'confirm',
+          handler: () => {
+            this.deleteAssignmentByID(assignment.id)
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
